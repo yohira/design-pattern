e638ed203c4b9a19afef9585e26984af0e0d368c
abs_san <yukito.sys.so@gmail.com>
2014-10-13 22:15:32 +0900

    テストケースのモック作成

--- Iterator/main/BookShelfIterator.java
+++ Iterator/main/BookShelfIterator.java
@@ -5,15 +5,23 @@ import java.util.Iterator;
 public class BookShelfIterator implements Iterator{
 	private BookShelf bookShelf;
 	private int index;
+	public BookShelfIterator(BookShelf bookShelf){
+		this.bookShelf=bookShelf;
+		this.index=0;
+	}
 	@Override
 	public boolean hasNext() {
-		// TODO 自動生成されたメソッド・スタブ
-		return false;
+		if(index<bookShelf.getLength()){
+			return true;
+		}else{
+			return false;
+		}
 	}
 	@Override
 	public Object next() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		Book book=bookShelf.getBookAt(index);
+		index++;
+		return book;
 	}
 	@Override
 	public void remove() {
---
+++ Iterator/test/Prac1_1Test.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Prac1_1Test {
+
+	@Test
+	public void test() {
+		fail("まだ実装されていません");
+	}
+
+}
